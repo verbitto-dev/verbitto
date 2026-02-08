91233a2a6447423b412e1d183f7610210d58b49e
nebuclaw <nebuclaw@gmail.com>
2026-02-08 13:34:51 +0900

    feat: add migration deployment script
    - Introduce `deploy.ts` for managing and automating migrations.

---
+++ migrations/deploy.ts
@@ -0,0 +1,8 @@
+// Migrations are an early feature. Currently, they're nothing more than this
+// temporary script, which can be used to deploy new programs.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const anchor = require("@coral-xyz/anchor");
+
+module.exports = async function (provider: any) {
+    anchor.setProvider(provider);
+};
