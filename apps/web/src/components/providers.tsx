a9b4c6eff116a4a76cfe466352171036f6628d26
nebuclaw <nebuclaw@gmail.com>
2026-02-12 19:03:14 +0900

    Change default theme to light mode

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -9,7 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
+      defaultTheme="light"
       disableTransitionOnChange
       enableColorScheme
       enableSystem
